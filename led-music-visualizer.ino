3f0285f3d21c6be33f916031c1c8c5f4a6b58627
Charlie Steenhagen <csteen1005@gmail.com>
2018-07-19 12:43:44 -0500

    Removed led count stuff
    Had to remove LED count stuff because dynamic memory allocation is not a good idea. Will just have to hard code the led count.

--- led-music-visualizer.ino
+++ led-music-visualizer.ino
@@ -7,12 +7,12 @@
 #define DC_One A0
 #define DC_Two A1 
 #define DATA_PIN     6
-#define DEFAULT_LEDS  60
+#define NUM_LEDS  60
 #define BRIGHTNESS  255
 #define LED_TYPE    WS2812
 #define COLOR_ORDER GRB
-#define BUTTONDOWN    11
-#define BUTTONUP      12
+#define BUTTON_DOWN    11
+#define BUTTON_UP      12
 
 // I'm seeing best results using band 4 to isolate the drumbeats
 #define DRUM_BAND 4
@@ -26,7 +26,7 @@
 #define FADE_DELAY 1
 
 //Define spectrum variables
-int NUM_LEDS = DEFAULT_LEDS;
+// int NUM_LEDS = DEFAULT_LEDS;
 int freq_amp;
 int Frequencies_One[7];
 int Frequencies_Two[7]; 
@@ -41,8 +41,8 @@ CRGB leds[NUM_LEDS];
 void setup() {
   Serial.begin(9600);
   
-  pinMode(BUTTONUP, INPUT_PULLUP);
-  pinMode(BUTTONDOWN, INPUT_PULLUP);
+  pinMode(BUTTON_UP, INPUT_PULLUP);
+  pinMode(BUTTON_DOWN, INPUT_PULLUP);
   
   //Set spectrum Shield pin configurations
   pinMode(STROBE, OUTPUT);
@@ -63,7 +63,10 @@ void setup() {
   delay(1);
   digitalWrite(RESET, LOW);
   
-  readLedCount();
+  // readLedCount();
+  
+  // CRGB temp[NUM_LEDS];
+  // leds = temp;
   
   FastLED.addLeds<LED_TYPE, DATA_PIN, COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
   FastLED.setBrightness(BRIGHTNESS);
@@ -74,7 +77,7 @@ void setup() {
 
 /**************************Main Function Loop*****************************/
 void loop() {
-  readButtons();
+  // readButtons();
   readFrequencies();
   setLeds();
   fadeLeds();
@@ -94,53 +97,53 @@ void setLeds(){
   } 
 }
 
-void readButtons(){
-  if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == LOW){
-    inputLedCount();   
-  }
-}
+// void readButtons(){
+//   if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == LOW){
+//     inputLedCount();   
+//   }
+// }
 
-void inputLedCount(){
-  int ledCount = NUM_LEDS;
-  while(1){
-    //TODO make sure this is the right color
-    fill_solid(leds, ledCount, CHSV(hue, 255, BRIGHTNESS));
-    if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == HIGH){ //TODO consider setting max led count
-      ledCount++;
-    }else if(digitalRead(BUTTON_UP) == HIGH && digitalRead(BUTTON_DOWN) == LOW && ledCount > 0){
-      ledCount--;
-    }else if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == LOW){
-      updateLedCount();
-      break;
-    }
-    delay(50);
-  }
-}
+// void inputLedCount(){
+//   int ledCount = NUM_LEDS;
+//   while(1){
+//     //TODO make sure this is the right color
+//     fill_solid(leds, ledCount, CHSV(hue, 255, BRIGHTNESS));
+//     if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == HIGH){ //TODO consider setting max led count
+//       ledCount++;
+//     }else if(digitalRead(BUTTON_UP) == HIGH && digitalRead(BUTTON_DOWN) == LOW && ledCount > 0){
+//       ledCount--;
+//     }else if(digitalRead(BUTTON_UP) == LOW && digitalRead(BUTTON_DOWN) == LOW){
+//       updateLedCount();
+//       break;
+//     }
+//     delay(50);
+//   }
+// }
 
-void updateLedCount(){
-  int ledCount = NUM_LEDS;
-  int multiplier = 0;
-  while(ledCount > 255){
-    ledCount -= 255;
-    multiplier++;
-  }
+// void updateLedCount(){
+//   int ledCount = NUM_LEDS;
+//   int multiplier = 0;
+//   while(ledCount > 255){
+//     ledCount -= 255;
+//     multiplier++;
+//   }
   
-  EEPROM.write(0, 1); //Tells our program that we have written to EEPROM once before
-  EEPROM.write(1, ledCount); //Writes the remaining led count
-  EEPROM.write(2, multiplier); //Writes the multplier to EEPROM
-}
+//   EEPROM.write(0, 1); //Tells our program that we have written to EEPROM once before
+//   EEPROM.write(1, ledCount); //Writes the remaining led count
+//   EEPROM.write(2, multiplier); //Writes the multplier to EEPROM
+// }
 
-void readLedCount(){
-  byte alreadySet = EEPROM.read(0);
-  byte value = EEPROM.read(1);
-  byte multiplier = EEPROM.read(2);
-  if(set == 0x01){
-    //can only save up to 255 in EEPROM, so we use a multiplier
-    NUM_LEDS = (255 * multiplier) + value;
-  }else{
-    updateLedCount(); //Writes our current NUM_LEDS to EEPROM
-  }
-}
+// void readLedCount(){
+//   byte alreadySet = EEPROM.read(0);
+//   byte value = EEPROM.read(1);
+//   byte multiplier = EEPROM.read(2);
+//   if(alreadySet == 0x01){
+//     //can only save up to 255 in EEPROM, so we use a multiplier
+//     NUM_LEDS = (255 * multiplier) + value;
+//   }else{
+//     updateLedCount(); //Writes our current NUM_LEDS to EEPROM
+//   }
+// }
 
 void fadeLeds(){
   if (fadeCountdown == 0) {
@@ -186,7 +189,7 @@ void printFrequenciesTwo(){
     Serial.print(" ");
     Serial.print(i);
     Serial.print(":");
-    Serial.print(Frequencies_Two);
+    Serial.print(Frequencies_Two[i]);
   }
   Serial.println("");
 }
@@ -201,17 +204,14 @@ void printSettings(){
   Serial.print("BRIGHTNESS: ");
   Serial.println(BRIGHTNESS); 
   
-  Serial.print("LED TYPE: ");
-  Serial.println(LED_TYPE); 
-  
   Serial.print("COLOR ORDER: ");
   Serial.println(COLOR_ORDER); 
   
   Serial.print("BUTTON UP: ");
-  Serial.println(BUTTONUP); 
+  Serial.println(BUTTON_UP); 
   
   Serial.print("BUTTON DOWN: ");
-  Serial.println(BUTTONDOWN); 
+  Serial.println(BUTTON_DOWN); 
   
   Serial.print("DRUM BAND: ");
   Serial.println(DRUM_BAND); 
